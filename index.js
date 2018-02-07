4dc6c121def8ef756c3acb677efdd7d45110bb46
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-07 16:20:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, name) {
+function holidayCountdown(days, name) {
   return `Happy ${holiday}, ${name}!`
 }
